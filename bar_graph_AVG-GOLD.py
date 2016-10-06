27a63b988681f09be4b38b348e426228f841746c
Yendall <yendallmax@gmail.com>
2016-10-06 19:11:59 +1100

    Comments and FileName

--- bar_graph_AVG-GOLD.py
+++ bar_graph_AVG-GOLD.py
@@ -1,32 +1,46 @@
 import plotly.plotly as py
 import plotly.graph_objs as go
 import json
-from sys import argv
 from collections import defaultdict
+
+# --begin_header--
+__author__ = "Max Yendall"
+__credits__ = "Plotly Data Plotting Services"
+__version__ = "1.0"
+__email__ = "s3436993@student.rmit.edu.au"
+# --end_header--
 
 with open('Aggregate/Years/data.json') as data_file:
     data = json.load(data_file)
 
+# Declare default dictionary objects for data
 d_gold = defaultdict(list)
 d_tone = defaultdict(list)
+
+# Dictionaries to hold aggregated data
 reduce_tone = defaultdict(list)
 reduce_gold = defaultdict(list)
 
+# Populate both dictionaries and handle duplicates
 for i in data['maps']:
     d_tone[i['Year']].append(i['Average_Tone'])
     d_gold[i['Year']].append(i['Goldstein'])
 
+# Perform lambda function on each list to obtain average values
 for k,v in d_tone.iteritems():
     reduce_tone[k] = reduce(lambda x, y: x + y, v) / len(v)
-
 for k, v in d_gold.iteritems():
     reduce_gold[k] = reduce(lambda x, y: x + y, v) / len(v)
 
+# Prompt for Username and API Key for Plotly services
 user_name = raw_input("What is your username?")
 API_key = raw_input("What is your API Key?")
 
+# Authenticate user to begin creating the graph
 py.sign_in(user_name,API_key)
-trace0 = go.Bar(
+
+# Map Tone values to Bar objects
+tone_trace = go.Bar(
     x=reduce_tone.keys(),
     y=reduce_tone.values(),
     name='Average Tone',
@@ -35,7 +49,8 @@ trace0 = go.Bar(
     )
 )
 
-trace1 = go.Bar(
+# Map Goldstein values to Bar objects
+goldstein_trace = go.Bar(
     x=reduce_gold.keys(),
     y=reduce_gold.values(),
     name='Average Goldstein',
@@ -44,7 +59,10 @@ trace1 = go.Bar(
     )
 )
 
-data = [trace0,trace1]
+# Map Bar objects to data array
+data = [tone_trace,goldstein_trace]
+
+# Ensure layout is designed well and readable
 layout = go.Layout(
     paper_bgcolor='#151515',
     plot_bgcolor='#151515',
@@ -54,5 +72,6 @@ layout = go.Layout(
     barmode='group',
 )
 
+# Plot data using layout configuration
 fig = go.Figure(data=data, layout=layout)
 py.iplot(fig, filename='angled-text-bar')
